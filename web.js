496337fb6720522f063fd92483e026ac0c8e874d
ulloa <samuel.ulloa@gmail.com>
2014-07-04 12:22:10 -0700

    fixed routing of page about

--- web.js
+++ web.js
@@ -2,10 +2,12 @@ require('newrelic');
 var express = require('express');
 var app = express();
 app.use(express.logger());
-app.use("/public", express.static(__dirname + '/public'));
+//app.use("/public", express.static(__dirname + '/public'));
 
 var fs = require('fs');
 var htmlfile = "index.html";
+app.set('views', __dirname + '/public');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(request, response) {
     var html = fs.readFileSync(htmlfile).toString();
@@ -13,7 +15,12 @@ app.get('/', function(request, response) {
 //  response.send(fs.readFileSync('index.html').tostring());
 });
 
+
 app.use(express.static(__dirname + '/public')); // just added for static files
+app.get('/about', function (req, res)
+{
+    res.render('about.html');
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
